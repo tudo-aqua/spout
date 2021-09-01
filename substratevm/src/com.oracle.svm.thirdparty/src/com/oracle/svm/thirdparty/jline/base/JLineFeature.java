c42dc18d76f5f85cc07a7035aec9f91c56ae76ed
Paul Wögerer <paul.woegerer@oracle.com>
2021-09-01 14:57:59 +0200

    Move JLineFeature into unique package
    (must not conflict with com.oracle.svm.thirdparty.jline from shadowed jline3)

--- substratevm/src/com.oracle.svm.thirdparty/src/com/oracle/svm/thirdparty/jline/base/JLineFeature.java
+++ substratevm/src/com.oracle.svm.thirdparty/src/com/oracle/svm/thirdparty/jline/base/JLineFeature.java
@@ -22,7 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package com.oracle.svm.thirdparty.jline;
+package com.oracle.svm.thirdparty.jline.base;
 
 // Checkstyle: allow reflection
 
