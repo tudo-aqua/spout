622e0b6b4418af560787bf994ef1d19dcad57dae
Malte Mues <mail.mues@gmail.com>
2022-10-10 13:57:00 +0200

    Disable the networking for now

---
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/substitutions/Target_java_net_Socket.java
@@ -0,0 +1,12 @@
+package com.oracle.truffle.espresso.substitutions;
+
+import com.oracle.truffle.espresso.meta.Meta;
+import tools.aqua.spout.SPouT;
+
+@EspressoSubstitutions
+public final class Target_java_net_Socket {
+    @Substitution(methodName = "<init>")
+    public static void __init(@JavaType(String.class) Object server, @JavaType(internalName = "I") Object port, @Inject Meta meta) {
+        SPouT.stopRecording("Networking not supported yet.", meta);
+    }
+}
