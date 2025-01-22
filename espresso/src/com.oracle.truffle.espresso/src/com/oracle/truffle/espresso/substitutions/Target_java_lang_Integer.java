4f09434402e76837593febea8b2a02c23a3371ea
Falk Howar <falk.howar@gmail.com>
2025-01-22 09:16:24 +0100

    disable integer cache

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/substitutions/Target_java_lang_Integer.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/substitutions/Target_java_lang_Integer.java
@@ -2,6 +2,9 @@ package com.oracle.truffle.espresso.substitutions;
 
 import com.oracle.truffle.espresso.meta.Meta;
 import com.oracle.truffle.espresso.runtime.StaticObject;
+import tools.aqua.spout.AnnotatedVM;
+import tools.aqua.spout.AnnotatedValue;
+import tools.aqua.spout.Annotations;
 import tools.aqua.spout.SPouT;
 
 @EspressoSubstitutions
@@ -10,5 +13,14 @@ public final class Target_java_lang_Integer {
     @Substitution
     public static int parseInt(@JavaType(String.class) StaticObject s, @Inject Meta meta){
         return SPouT.parseInt(s, meta);
+    }
+
+    @Substitution(passAnnotations = true)
+    public static @JavaType(Integer.class) StaticObject valueOf(@JavaType(internalName = "I") Object i, @Inject Meta meta) {
+        StaticObject o = meta.java_lang_Integer.allocateInstance();
+        meta.java_lang_Integer_value.set(o, AnnotatedValue.value(i));
+        AnnotatedVM.setFieldAnnotation(o, meta.java_lang_Integer_value,
+                AnnotatedValue.svalue(i));
+        return o;
     }
 }
