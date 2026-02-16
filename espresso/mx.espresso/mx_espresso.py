b55ff5ec7a071803ec18f15ab8a51e1b4b2f7360
Falk Howar <falk.howar@gmail.com>
2026-02-16 23:45:22 +0100

    experimental build fix linux

--- espresso/mx.espresso/mx_espresso.py
+++ espresso/mx.espresso/mx_espresso.py
@@ -117,8 +117,8 @@ def javavm_build_args():
         result.append('-H:-IncludeLanguageResources')
     if mx.is_linux() and mx.get_os_variant() != "musl":
         result += [
-            '-Dpolyglot.image-build-time.PreinitializeContexts=java',
-            '-Dpolyglot.image-build-time.PreinitializeContextsWithNative=true',
+#            '-Dpolyglot.image-build-time.PreinitializeContexts=java',
+#            '-Dpolyglot.image-build-time.PreinitializeContextsWithNative=true',
         ]
     return result
 
