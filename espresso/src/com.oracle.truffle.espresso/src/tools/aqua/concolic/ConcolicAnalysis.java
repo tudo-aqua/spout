b71b65c17a694987101093fb5abb8c68474bebd6
Falk Howar <falk.howar@gmail.com>
2026-06-25 11:03:15 +0200

    same check different place

--- espresso/src/com.oracle.truffle.espresso/src/tools/aqua/concolic/ConcolicAnalysis.java
+++ espresso/src/com.oracle.truffle.espresso/src/tools/aqua/concolic/ConcolicAnalysis.java
@@ -1160,6 +1160,7 @@ public class ConcolicAnalysis implements Analysis<Expression> {
             //  (as I think they cannot become true and break getClass()....() calls)
             AuxiliaryVariable atom = (AuxiliaryVariable) a;
             if (atom.getType() == STRING || atom.toString().endsWith(".cls")) return;
+            if (atom.getType() != OBJECT) SPouT.stopRecordingWithoutMeta("Only objects can be null.");
         }
 
         Expression expr = new ComplexExpression(OBJECT_IS_NULL, a, Constant.NULL);
